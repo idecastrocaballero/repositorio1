e729d2f8e1e25b76682aabbba6ab6a670e942fb1
Iván de Castro <Ivn de Castro@10.34.82.53>
2017-12-11 15:12:50 +0100

    123

---
+++ src/operaciones.java
@@ -0,0 +1,17 @@
+
+public class operaciones {
+	public static void main(String[] args) {
+int n1=2 , n2;
+n2= n1 * n1;
+System.out.println(n2);
+n2= n1 - n1;
+System.out.println(n2);
+n2= n1 + n1;
+System.out.println(n2);
+n2= n1 / n1;
+System.out.println(n2);
+n2= n1 % n1;
+System.out.println(n2);
+
+}
+}
